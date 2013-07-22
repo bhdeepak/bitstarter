0cefa1111bf7d856edda0827845495347b5df318
Deepak BH <dbangalo@usc.edu>
2013-07-22 08:16:02 +0000

    read from index and print

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fsi = require('fs');
 var app = express();
+var buffer = new Buffer(30);
 app.use(express.logger());
 
+buffer.write(fs.readFileSync(./index.html))
+
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
