7e09558d61f08ce44e04f3ab91c2f1d0b3f3ee7a
Deepak BH <dbangalo@usc.edu>
2013-07-24 13:47:28 +0000

    final grader submission

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -61,12 +62,32 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var response2file = function(result, response) {
+    if(result instanceof Error) {
+	console.error('Error: ' + util.format(response.message));
+    } else {
+	fs.writeFileSync("url_source.html", result);
+    }
+
+}
+
+var pass = function() {
+	;
+}
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_link>', 'A URL link', pass(), HTMLFILE_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    if(program.file) {
+    	var checkJson = checkHtmlFile(program.file, program.checks);
+    }
+    else if(program.url) {
+	rest.get(program.url).on('complete', response2file);
+	var checkjson = checkHtmlFile("url_source.html", program.checks);
+    }
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
